f1e717d3e59ec12f596d46c3176b9a8bcdcaa3fe
zhangxin <112859811@qq.com>
2020-06-08 01:13:05 +0800

    新增用例页面2

--- src/components/interfaceTest/InterfaceCase.vue
+++ src/components/interfaceTest/InterfaceCase.vue
@@ -11,7 +11,7 @@
             </el-breadcrumb>
         </div>
         <!-- 新增用例区域 -->
-        <el-card>
+        <el-card v-loading="loading">
             <el-form ref="createCaseRef" :model="createCaseForm" :rules="createCaseRules">
                 <el-row>
                     <el-col :span="5">
@@ -30,41 +30,44 @@
                         </el-form-item>
                     </el-col>
                     <el-col :span="6">
-                        <el-form-item style="margin-left: 10px" prop="case_name">
+                        <el-form-item style="margin-left: 10px">
                             <el-input placeholder="请输入用例名称" v-model="createCaseForm.test_case.case_name" clearable>
                                 <template slot="prepend">用例名称:</template>
                             </el-input>
                         </el-form-item>
                     </el-col>
-                    <el-col :span="6" style="margin-left: 10px">
-                        <el-button type="primary" @click="createTestCase">
-                            提交用例
-                        </el-button>
+                    <el-col :span="6">
+                        <el-form-item style="margin-left: 10px">
+                            <el-input placeholder="请输入用例描述" v-model="createCaseForm.test_case.case_desc" clearable>
+                                <template slot="prepend">用例描述:</template>
+                            </el-input>
+                        </el-form-item>
                     </el-col>
                 </el-row>
                 <!-- 动态输入框-->
                 <el-card>
                     <div v-for="(item, index) in createCaseForm.test_step" :key="index">
                         <el-row>
-                            <el-col :span="3">
-                                <el-form-item>
-                                    <el-input v-model="item.num" clearable :value="index" disabled>
-                                        <template slot="prepend">序号:</template>
-                                    </el-input>
+                            <el-col :span="1">
+                                <el-form-item v-model="item.step_num=index">
+                                    <el-tag>序号:{{index}}</el-tag>
+<!--                                    <el-input v-model="item.step_num" clearable :value="index" disabled>-->
+<!--                                        <template slot="prepend">序号:</template>-->
+<!--                                    </el-input>-->
                                 </el-form-item>
                             </el-col>
                             <el-col :span="6">
                                 <el-form-item style="margin-left: 10px"
-                                              :prop="'test_step.' + index + '.name'"
+                                              :prop="'test_step.' + index + '.step_name'"
                                               :rules="{ required: true, message: '请输入步骤名称', trigger: 'blur'}">
-                                    <el-input v-model="item.name" clearable>
+                                    <el-input v-model="item.step_name" clearable>
                                         <template slot="prepend">步骤名称:</template>
                                     </el-input>
                                 </el-form-item>
                             </el-col>
                             <el-col :span="6">
                                 <el-form-item style="margin-left: 10px">
-                                    <el-input v-model="item.desc" clearable>
+                                    <el-input v-model="item.step_desc" clearable>
                                         <template slot="prepend">步骤描述:</template>
                                     </el-input>
                                 </el-form-item>
@@ -99,7 +102,7 @@
                             </el-col>
                         </el-row>
                         <el-row>
-                            <el-col :span="3">
+                            <el-col :span="4">
                                 <el-form-item label="请求方式" :prop="'test_step.' + index + '.method'"
                                               :rules="{ required: true, message: '请选择请求方式', trigger: 'change'}">
                                     <el-select v-model="item.method" placeholder="请选择" style="width: 120px">
@@ -111,7 +114,7 @@
                                 </el-form-item>
                             </el-col>
                             <el-col :span="6">
-                                <el-form-item style="margin-left: 10px" :prop="'test_step.' + index + '.url'"
+                                <el-form-item style="margin-left: -40px" :prop="'test_step.' + index + '.url'"
                                               :rules="{ required: true, message: '请输入路径', trigger: 'blur'}">
                                     <el-input v-model="item.url" clearable>
                                         <template slot="prepend">PATH</template>
@@ -119,8 +122,10 @@
                                 </el-form-item>
                             </el-col>
                             <el-col :span="6">
-                                <el-form-item style="margin-left: 10px" :prop="'test_step.' + index + '.variable'"
-                                              :rules="{ required: true, message: '请输入变量,格式:$variable$ $variable$ $variable$', trigger: 'blur'}">
+                                <el-form-item style="margin-left: 10px">
+<!--                                              :prop="'test_step.' + index + '.variable'"-->
+<!--                                              :rules="{ required: true, message: '请输入变量,格式:$variable$ $variable$ $variable$', trigger: 'blur'}">-->
+
                                     <el-input v-model="item.variable" clearable>
                                         <template slot="prepend">变量</template>
                                     </el-input>
@@ -139,12 +144,6 @@
                                       inactive-value= 0>
                                     </el-switch>
                                 </el-form-item>
-                            </el-col>
-                            <el-col :span="2">
-                                <el-button @click="deleteItem(item, index)" type="danger" round
-                                           size="mini" style="margin-left: 20px">
-                                    删除该步骤
-                                </el-button>
                             </el-col>
                         </el-row>
                         <el-row>
@@ -198,12 +197,37 @@
                             <el-col :span="8">
                                 <el-card class="box-card" style="margin-left: 10px; height: 210px">
                                     <div slot="header" class="clearfix">
-                                        <span>服务器返回内容</span>
+                                        <template v-model="res_c">
+                                            <el-tag
+                                              :type="res_c === false ? 'danger' : 'success'"
+                                              disable-transitions>状态码判定:{{res_c}}</el-tag>
+                                        </template>
+                                        <template v-model="res_r" style="margin-left: 10px">
+                                            <el-tag
+                                              :type="res_r === false ? 'danger' : 'success'"
+                                              disable-transitions>响应内容判定:{{res_r}}</el-tag>
+                                        </template>
+
+                                        <el-button style="float: right; padding: 3px 0" type="text"
+                                                   @click="drawer = true">
+                                            显示更多
+                                        </el-button>
                                     </div>
                                     <div v-model="response" class="text item">
                                         {{response}}
                                     </div>
                                 </el-card>
+                            </el-col>
+                            <el-col :span="2">
+                                <el-button type="success"
+                                           style="margin-left: 20px; margin-top: 20px" round
+                                           @click="testCase(index)">
+                                    测试
+                                </el-button>
+                                <el-button @click="deleteItem(item, index)" type="danger" round
+                                           style="margin-left: 20px; margin-top: 20px">
+                                    删除该步骤
+                                </el-button>
                             </el-col>
                         </el-row>
                         <el-row>
@@ -213,10 +237,18 @@
                         </el-row>
                     </div>
                     <el-button @click="addItem" type="primary" style="margin-left: 20px">增加下一步</el-button>
-                    <el-button type="success" @click="testCase">测试</el-button>
+                        <el-button type="primary" @click="createTestCase">
+                            提交用例
+                        </el-button>
                 </el-card>
             </el-form>
         </el-card>
+        <el-drawer
+          title="我是标题"
+          :visible.sync="drawer"
+          :with-header="false">
+            <span>{{response}}</span>
+        </el-drawer>
     </div>
 </template>
 
@@ -227,6 +259,9 @@
             return {
                 activeName2: 'first',
                 response: '点击测试按钮后获取内容',
+                drawer: false,
+                res_c: false,
+                res_r: false,
                 // 项目列表缓存
                 projectList: [],
                 host: null,
@@ -236,19 +271,22 @@
                     project_id: null,
                     test_case: {
                         case_name: null,
-                        desc: null
+                        case_desc: null
                     },
                     test_step: []
                 },
                 // 表单验证
                 createCaseRules: {
                     project_id: [{ required: true, message: '请选择项目', trigger: 'change'}],
-                    case_name: [{ required: true, message: '请输入用例名称', trigger: 'blur'}]
-                }
+                    case_name: [{ required: true, message: '请输入用例名称', trigger: 'blur'},
+                        {min:1, max:255, message: '长度需要大于1或者小于250', trigger: 'blur'}]
+                },
+                loading: false
             }
         },
         created() {
             this.getCreatePage()
+              this.addItem()
             },
         methods: {
 
@@ -256,14 +294,28 @@
                 for(let i=0;i<this.projectList.length;i++){
                     if(this.projectList[i].id===val){
                         this.host=this.projectList[i].host
-                        console.log(this.host)
+                        // console.log(this.host)
                     }
                 }
             },
             addItem () {
                 this.createCaseForm.test_step.push({
-                    num: null,
-                    name: null
+                    step_num: null,
+                    step_name: null,
+                    step_desc: null,
+                    set_up: null,
+                    tear_down: null,
+                    method: null,
+                    url: null,
+                    variable: null,
+                    need_assert: null,
+                    assert_method: null,
+                    headers: null,
+                    params: null,
+                    form_data: null,
+                    json_data: null,
+                    exp_statue: null,
+                    exp_extract: null
                 })
             },
             deleteItem (item, index) {
@@ -278,15 +330,55 @@
                 // console.log(res)
             },
             createTestCase() {
-                console.log(this.createCaseForm)
-            },
-            testCase() {
                 this.$refs.createCaseRef.validate(async valid => {
                     if (!valid) {
                         return this.$message.error('缺少必填项!')
                     }
-                    const {data: res} = this.$http.post('interface/test_step/', this.createCaseForm)
-                    console.log(this.createCaseForm)
+                    // this.loading = true
+                    const {data: res} = await this.$http.post('interface/create_case/', this.createCaseForm)
+                    if (res.meta.status !== 200) {
+                        this.loading = false
+                        return this.$message.error(res.meta.msg)
+                    }
+                    this.loading = false
+                    return this.$message.success(res.meta.msg)
+
+            })
+            },
+            testCase(index) {
+                this.$refs.createCaseRef.validate(async valid => {
+                    if (!valid) {
+                        return this.$message.error('缺少必填项!')
+                    }
+                    this.loading = true
+                    const testData = {
+                        project_id: this.createCaseForm.project_id,
+                        host: this.host,
+                        step_num: index,
+                        step_name: this.createCaseForm.test_step[index].step_name,
+                        step_desc: this.createCaseForm.test_step[index].step_desc,
+                        set_up: this.createCaseForm.test_step[index].set_up,
+                        tear_down: this.createCaseForm.test_step[index].tear_down,
+                        url: this.createCaseForm.test_step[index].url,
+                        method: this.createCaseForm.test_step[index].method,
+                        variable: this.createCaseForm.test_step[index].variable,
+                        headers: this.createCaseForm.test_step[index].headers,
+                        params: this.createCaseForm.test_step[index].params,
+                        form_data: this.createCaseForm.test_step[index].form_data,
+                        json_data: this.createCaseForm.test_step[index].json_data,
+                        need_assert: this.createCaseForm.test_step[index].need_assert,
+                        exp_statue: this.createCaseForm.test_step[index].exp_statue,
+                        exp_extract: this.createCaseForm.test_step[index].exp_extract
+                    }
+                    const {data: res} = await this.$http.post('interface/to_test_step/', testData)
+                    if (res.meta.status !== 200) {
+                        this.loading = false
+                        return this.$message.error(res.meta.msg)
+                    }
+                    this.response = res.data
+                    this.res_c = res.data.assert.code
+                    this.res_r = res.data.assert.response
+                    this.loading = false
 
                 })
             }
